2ff5cabbf3aa0a5b08c9df5b27e56764538479a1
Vishnu-Adi <111406816+Vishnu-Adi@users.noreply.github.com>
2024-07-24 00:41:26 +0530

    remove nav

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,29 +9,12 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <nav className="bg-gray-800 p-4">
-          <ul className="flex space-x-4 text-white">
-            <li>
-              <Link to="/" className="hover:underline">Home</Link>
-            </li>
-            <li>
-              <Link to="/login" className="hover:underline">Login</Link>
-            </li>
-            <li>
-              <Link to="/register" className="hover:underline">Register</Link>
-            </li>
-            <li>
-              <Link to="/recommendations" className="hover:underline">Movie Recommendations</Link>
-            </li>
-          </ul>
-        </nav>
-
         <main className="p-4">
           <Routes>
-            <Route path="/" element={<Home />} />
+            
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
-            <Route path="/recommendations" element={<MovieRecommendations />} /> {/* Added route */}
+            <Route path="/recommendations" element={<MovieRecommendations />} /> 
           </Routes>
         </main>
       </div>
@@ -39,8 +22,6 @@ function App() {
   );
 }
 
-function Home() {
-  return <h2 className="text-2xl font-bold">Welcome to the Movie Recommendation Site</h2>;
-}
+
 
 export default App;
